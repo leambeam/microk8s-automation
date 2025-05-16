6a6802b9cf4a0bd1b9390c3f878f541f8faf7f12
Grigory Provodin <ab0229@student.jamk.fi>
2025-05-17 00:06:21 +0300

    feat: Update Heat deployment script

--- scripts/heat-deploy.sh
+++ scripts/heat-deploy.sh
@@ -3,5 +3,12 @@ openstack stack create -t heat/main.yaml -e heat/default-vars.yaml vm-stack
 # Wait for stack to allocate and assosiate floating ips
 sleep 60
 
-openstack stack output show vm-stack ubuntu_floating_ip -f value -c output_value
-openstack stack output show vm-stack rocky_floating_ip -f value -c output_value
+
+ubuntu=$(openstack stack output show vm-stack ubuntu_floating_ip -f value -c output_value)
+rocky=$(openstack stack output show vm-stack rocky_floating_ip -f value -c output_value)
+
+echo "Ubuntu floating ip: ${ubuntu}"
+echo "Rocky floating ip: ${rocky}"
+
+
+openstack stack show vm-stack -f yaml -c "stack_status"
